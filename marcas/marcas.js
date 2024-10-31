b7fcbebafe0b9c0a2709e70e6a0100ed31aaee6f
KauaFSB <kauaflp234@gmail.com>
2024-10-30 22:55:13 -0300

    trocado o layout da pagina de marcas

--- marcas/marcas.js
+++ marcas/marcas.js
@@ -1,26 +1,9 @@
-gsap.registerPlugin(ScrollTrigger, ScrollSmoother, SplitText);
+const nextBtn = document.getElementById('next-btn');
+const containerWrapper = document.querySelector('.agrupa-container');
+let currentContainer = 0; 
 
-// create the smooth scroller FIRST!
-const smoother = ScrollSmoother.create({
- wrapper: "#wrapper",
- content: "#content",
- smooth: 1,
- normalizeScroll: true, // prevents address bar from showing/hiding on most devices, solves various other browser inconsistencies
- ignoreMobileResize: true, // skips ScrollTrigger.refresh() on mobile resizes from address bar showing/hiding
- effects: true,
- preventDefault: true
+nextBtn.addEventListener('click', () => {
+      currentContainer = (currentContainer + 1) % 3;
+  
+  containerWrapper.style.transform = `translateX(-${currentContainer * 100}%)`;
 });
-
-gsap.set(".heading", {
- yPercent: -150,
- opacity: 1
-});
-
-let tl = gsap.timeline();
-let mySplitText = new SplitText("#split-stagger", { type: "words,chars" });
-let chars = mySplitText.chars;
-
-chars.forEach((char, i) => {
- smoother.effects(char, { speed: 1, lag: (i + 1) * 0.1 });
-});
-
