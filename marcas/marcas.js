d24eb66d53d3bc408dd031d2b05dde501defe322
KauaFSB <kauaflp234@gmail.com>
2024-11-01 15:50:38 -0300

    ajustado cores e feito o banner

--- marcas/marcas.js
+++ marcas/marcas.js
@@ -1,11 +1,31 @@
-const nextBtn = document.getElementById('next-btn');
+const proximoBtn = document.getElementById('proximo-btn');
+const anteriorBtn = document.getElementById('anterior-btn');
 const containerWrapper = document.querySelector('.agrupa-container');
-let currentContainer = 0; 
+let containerAtual = 0; 
 
-nextBtn.addEventListener('click', () => {
-      currentContainer = (currentContainer + 1) % 3;
+proximoBtn.addEventListener('click', () => {
+      containerAtual = (containerAtual + 1) % 3;
   
   containerWrapper.style.transition = 'transform 0.5s ease-in-out';
-  containerWrapper.style.transform = `translateX(-${currentContainer * 100}%)`;
+  containerWrapper.style.transform = `translateX(-${containerAtual * 100}%)`;
 });
 
+anteriorBtn.addEventListener('click', () => {
+  containerAtual = (containerAtual - 1 + 3) % 3;
+
+  containerWrapper.style.transition = 'transform 0.5s ease-in-out';
+  containerWrapper.style.transform = `translateX(-${containerAtual * 100}%)`;
+});
+
+const banner = document.querySelector('.banner');
+
+window.addEventListener('scroll', () => {
+  const posicaoScroll = window.scrollY;
+
+  const minScale = 0.5;
+  const maxScale = 1;
+
+  const valorEscala = Math.max(minScale, maxScale - posicaoScroll / 3000);
+
+  banner.style.transform = `scale(${valorEscala})`;
+});
